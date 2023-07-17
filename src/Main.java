48f406ebdbb2e98b3a1909f5f8aa8a7fa2d0dfa5
samuelbozio <samuelbozio@hotmail.com>
2023-07-17 20:39:38 -0300

    associacao um pra um em javas

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Jogador jogador1 = new Jogador("Samuel");
+        Time time = new Time("Brazil");
+
+        jogador1.setTime(time);
+        jogador1.imprime();
+
+
+
+    }
+}
